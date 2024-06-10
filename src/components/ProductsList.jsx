756e41b66ec9c7f8cd838ce35e71eaff5d47fa3a
Maïssane Abdallah <abd.maissane@gmail.com>
2024-06-10 05:38:45 +0200

    correction bugs

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
-import {useSelector, useDispatch} from "react-redux"
-import {getProductsList} from "../features/products.js"
-import { addOneToCart } from '../features/cart.js'
-
+import { useSelector, useDispatch } from "react-redux"
+import { getProductsList } from "../features/products"
+import { addOneToCart } from "../features/cart"
 export default function ProductsList() {
-    const products = useSelector(state => state.products)
-    const dispatch = useDispatch()
+  const products = useSelector(state => state.products)
+  const dispatch = useDispatch()
 
-    if (!products.item){
-        dispatch(getProductsList())
-    }
+  if (!products.items) {
+    dispatch(getProductsList())
+  }
+  console.log(products)
   return (
     <div className="px-6">
-        <h1 className="text-slate-100 text-2xl mb-6">Here are our products</h1>
-        <ul className="grid min-[500px]:grid-cols-2 md:grid-cols-3 gap-4">
-            {products.item && products.items.map(el => (
-                <li key={el.id} className="p-4 bg-slate-200 rounded">
-                    <img 
-                    className="mb-4"
-                    src={`/public/images/${el.img}.png`} 
-                    alt={el.title}/>
-                    <div className="flex justify-between items-center mb-6">
-                        <p className="text-slate-700 text-lg">{el.title}</p>
-                        <p className="text-slate-900 font-bold">{el.price}</p>
-                    </div>
-                    <button 
-                    onClick={() => dispatch(addOneToCart(el.id))}
-                    className={`${
+      <h1 className="text-slate-100 text-2xl mb-6">Here are our products</h1>
+      <ul className="grid min-[500px]:grid-cols-2 md:grid-cols-3 gap-4">
+        {products.items &&
+          products.items.map(el => (
+            <li key={el.id} className="p-4 bg-slate-200 rounded">
+              <img
+                className="mb-4"
+                src={`/images/${el.img}.png`}
+                alt={el.title}
+              />
+              <div className="flex justify-between items-center mb-6">
+                <p className="text-slate-700 text-lg">{el.title}</p>
+                <p className="text-slate-900 font-bold">{el.price}</p>
+              </div>
+              <button
+                onClick={() => dispatch(addOneToCart(el.id))}
+                className={`${
                   el.picked ? "bg-green-700" : "bg-slate-600"
-                } w-full text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 mr-2`}>
-                        {el.picked ? "Item picked !" : "Add to cart"}
-                    </button>
-                </li>
-            ))}
-        </ul>
+                } w-full text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 mr-2`}
+              >
+                {el.picked ? "Item picked ✔️" : "Add to cart"}
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
